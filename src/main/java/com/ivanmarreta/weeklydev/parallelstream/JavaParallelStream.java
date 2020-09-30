1f74a3a900f6733767b96c233d01c7f5e5d676f5
Ivan Marrêta <ivanmarreta@gmail.com>
2020-10-01 00:54:11 +0100

    initialize with sequencial style code

--- src/main/java/com/ivanmarreta/weeklydev/parallelstream/JavaParallelStream.java
+++ src/main/java/com/ivanmarreta/weeklydev/parallelstream/JavaParallelStream.java
@@ -1,4 +1,22 @@
 package com.ivanmarreta.weeklydev.parallelstream;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class JavaParallelStream {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        //find total of triple of odd numbers
+        int total = 0;
+
+        for (int i : numbers) {
+            if(i % 2 != 0){
+                total += i*3;
+            }
+        }
+
+        System.out.println(total);
+    }
 }
