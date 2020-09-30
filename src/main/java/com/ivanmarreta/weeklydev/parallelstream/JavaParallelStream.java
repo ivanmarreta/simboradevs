1682330a07fad6aef1f5951646b6443731c695e6
Ivan Marrêta <ivanmarreta@gmail.com>
2020-10-01 00:59:35 +0100

    using java 8 implementation ComplatableFuture to show a new way to do the same

--- src/main/java/com/ivanmarreta/weeklydev/parallelstream/JavaParallelStream.java
+++ src/main/java/com/ivanmarreta/weeklydev/parallelstream/JavaParallelStream.java
@@ -2,14 +2,16 @@ package com.ivanmarreta.weeklydev.parallelstream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class JavaParallelStream {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
         //find total of triple of odd numbers
-        new Thread(() -> {
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> {
             int total = 0;
 
             for (int i : numbers) {
@@ -18,7 +20,11 @@ public class JavaParallelStream {
                 }
             }
 
-            System.out.println(total);
-        }).start();
+            return total;
+        });
+
+        Integer total = completableFuture.get();
+
+        System.out.println(total);
     }
 }
